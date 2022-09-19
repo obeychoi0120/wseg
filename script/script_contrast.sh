4bd1b4df7196d681717575e61dccdd77028b7771
suhyunyoon <billy555@naver.com>
2022-09-19 14:53:34 +0900

    Change bool argument according to changed contrast_infer.py

--- script/script_contrast.sh
+++ script/script_contrast.sh
@@ -13,18 +13,18 @@ SESSION=resnet38_contrast
 BASE_WEIGHT=${WEIGHT_ROOT}/ilsvrc-cls_rna-a1_cls1000_ep-0001.params
 
 
-# train classification network with Contrastive Learning
-CUDA_VISIBLE_DEVICES=${GPU} python3 contrast_train.py \
-  --session ${SESSION} \
-  --network network.${BACKBONE} \
-  --data_root ${IMG_ROOT} \
-  --saliency_root ${SAL_ROOT} \
-  --weights ${BASE_WEIGHT} \
-  --crop_size 448 \
-  --tau 0.4 \
-  --max_iters 10000 \
-  --iter_size 2 \
-  --batch_size 8
+# # train classification network with Contrastive Learning
+# CUDA_VISIBLE_DEVICES=${GPU} python3 contrast_train.py \
+#   --session ${SESSION} \
+#   --network network.${BACKBONE} \
+#   --data_root ${IMG_ROOT} \
+#   --saliency_root ${SAL_ROOT} \
+#   --weights ${BASE_WEIGHT} \
+#   --crop_size 448 \
+#   --tau 0.4 \
+#   --max_iters 10000 \
+#   --iter_size 2 \
+#   --batch_size 8
 
 
 # # Evaluate classification network
@@ -42,23 +42,23 @@ CUDA_VISIBLE_DEVICES=${GPU} python3 contrast_train.py \
 #   --batch_size 8
 
 
-# 2. inference CAM
-DATA=train_aug # train / train_aug
-TRAINED_WEIGHT=train_log/${SESSION}/checkpoint_contrast.pth
+# # 2. inference CAM
+# DATA=train_aug # train / train_aug
+# TRAINED_WEIGHT=train_log/${SESSION}/checkpoint_contrast.pth
 
-CUDA_VISIBLE_DEVICES=${GPU} python3 contrast_infer.py \
-    --infer_list data/voc12/${DATA}_id.txt \
-    --img_root ${IMG_ROOT} \
-    --network network.${BACKBONE} \
-    --weights ${TRAINED_WEIGHT} \
-    --thr 0.22 \
-    --n_gpus 4 \
-    --n_processes_per_gpu 1 1 1 1 \
-    --cam_png train_log/${SESSION}/result/cam_png \
-    --cam_npy train_log/${SESSION}/result/cam_npy \
-    --crf train_log/${SESSION}/result/crf_png\
-    --crf_t 5 \
-    --crf_alpha 8
+# CUDA_VISIBLE_DEVICES=${GPU} python3 contrast_infer.py \
+#     --infer_list data/voc12/${DATA}_id.txt \
+#     --img_root ${IMG_ROOT} \
+#     --network network.${BACKBONE} \
+#     --weights ${TRAINED_WEIGHT} \
+#     --thr 0.22 \
+#     --n_gpus 4 \
+#     --n_processes_per_gpu 1 1 1 1 \
+#     --cam_png train_log/${SESSION}/result/cam_png \
+#     --cam_npy train_log/${SESSION}/result/cam_npy \
+#     --crf train_log/${SESSION}/result/crf_png\
+#     --crf_t 5 \
+#     --crf_alpha 8
 
 # 3. evaluate CAM
 GT_ROOT=${DATASET_ROOT}/SegmentationClassAug/
@@ -71,7 +71,7 @@ CUDA_VISIBLE_DEVICES=${GPU} python3 eval.py \
     --comment $SESSION \
     --logfile train_log/${SESSION}/result/${DATA}.log \
     --type npy \
-    --curve True
+    --curve 
     # Use curve when type=npy
 
 # 4. Generate Segmentation pseudo label
